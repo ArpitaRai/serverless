05701f51bffdeabb402d6b55362cbd4ee358074b
Arpita Rai <arpitarai@Arpitas-MacBook-Pro.local>
2024-03-26 14:17:24 -0400

    name change of entry point

--- src/serverless/email-verification.js
+++ src/serverless/email-verification.js
@@ -4,7 +4,7 @@ import EamilTracker from '../models/email-tracker.js';
 
 // Register a CloudEvent callback with the Functions Framework that will
 // be executed when the Pub/Sub trigger topic receives a message.
-functions.cloudEvent('helloPubSub', async (cloudEvent) => {
+functions.cloudEvent('userVerification', async (cloudEvent) => {
   try {
     // The Pub/Sub message is passed as the CloudEvent's data payload.
     const base64name = cloudEvent.data.message.data;
